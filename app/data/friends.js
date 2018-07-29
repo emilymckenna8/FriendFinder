3aecae5cce8e0e974edea72d867dd6e2fd09477e
Emily McKenna <emilymckenna08@gmail.com>
2018-07-29 09:06:22 -0400

    added initial friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,93 @@
 var friendsSet = [
-
-
+    {
+        name: "Antonio",
+        photo: "http://www.gstatic.com/tv/thumb/persons/5885/5885_v9_ba.jpg",
+        scores: [
+            1,
+            3,
+            4,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            2     
+        ]
+    },
+    {
+        name: "DJ Khaled",
+        photo: "https://sslb.ulximg.com/image/405x405/artist/1392850906_2f16e083616376c167fda25befb0472c.jpg/92baae15e305323989676c75448cf4fa/1392850906_dj_khaled_27.jpg",
+        scores: [
+            1,
+            3,
+            2,
+            2,
+            3,
+            5,
+            5,
+            5,
+            5,
+            5     
+        ]    
+        
+    },
+    {
+        name: "John Mulaney",
+        photo: "https://s1.ticketm.net/tm/en-us/dam/a/fa0/519a6e0b-6e12-46b5-844e-f6d64b783fa0_366981_CUSTOM.jpg",
+        scores: [
+            1,
+            2,
+            1,
+            5,
+            4,
+            5,
+            1,
+            5,
+            2,
+            5     
+        ]    
+        
+    },
+    {
+        name: "Carly Rae Jepsen",
+        photo: "https://www.out.com/sites/out.com/files/2015/09/30/carly-rae-jepsen_0.jpg",
+        scores: [
+            2,
+            2,
+            1,
+            2,
+            2,
+            3,
+            1,
+            3,
+            2,
+            5     
+        ]    
+        
+    },
+    {
+        name: "Barack Obama",
+        photo: "https://s3.amazonaws.com/static.politifact.com/politifact/photos/AP_18197454081148.jpg",
+        scores: [
+            2,
+            2,
+            1,
+            2,
+            2,
+            3,
+            1,
+            3,
+            2,
+            5     
+        ]    
+        
+    },
 
 ]
+
+
+
+
 
 module.exports = friendsSet;
